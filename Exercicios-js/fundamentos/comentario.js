4e59c2769035bd6edfd36c0f7e0d9007c202e052
Pedro Henrique <pedrohenrique.midas@gmail.com>
2020-04-07 14:37:18 -0300

    Organização e Comentários

---
+++ Exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,15 @@
+//Comentário de uma linha
+console.log('Linha1')
+
+/*
+Comentário de 
+multiplas Linhas
+*/
+console.log('Linha 2')
+
+/*
+* Comentário de 
+* multiplas linhas
+* com *
+*/
+Console.log('Linha 3')
