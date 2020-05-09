7ca36063ee23ec0971279e0fbde5383433e2bfcd
Pedro Henrique <pedrohenrique.midas@gmail.com>
2020-05-09 13:18:29 -0300

    Exercício Proposto
    Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.

---
+++ Exercicios-js/ExercicioProposto/exercicio03.js
@@ -0,0 +1,13 @@
+/*
+* 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function potenciacao (base, expoente) {
+    return Math.pow(base, expoente)
+}
+
+console.log('Curso Web Moderno - Exercícios Javascript')
+console.log('Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.')
+
+console.log(potenciacao(7,2))
+console.log(potenciacao(2, 10))
